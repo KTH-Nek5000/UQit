3ec8d1976226f5ce345f8978759aee354fed7f58
Saleh <salehr@kth.se>
2020-08-10 15:02:01 +0200

    bugs fixed, Clenshaw-Curtis added

--- core/sampling.py
+++ core/sampling.py
@@ -7,7 +7,9 @@
 #
 import sys
 import numpy as np
+import math as mt
 import nodes
+import pce
 #
 #
 #//////////////////////////
@@ -35,19 +37,31 @@ def sampler_1d(range_,nSamp,sampType):
        range_: list of length 2, admissible range of parameters
        nSamp: Number of samples
        sampType: The method to drawing the samples (nodes)
-                 'random', 'uniform', 'GL', 'Clenshaw'
+                 'random', 'uniform', 'GL', 'Clenshaw', 'Clenshaw-Curtis'
+    Outputs:
+       qNodes: samples of size nSamp over range_ 
     """
     p=len(range_)
+    xi_len=1.
+    xi0=0.
     if sampType=='random':
-       xi=np.random.uniform(0,1,size=[nSamp])
+       xi=np.random.uniform(0,1,size=[nSamp-2])
+       xi=np.concatenate([[0],xi,[1]])
     elif sampType =='uniform':
-       xi=np.linspace(range_[0],range_[1],nSamp)
+       xi=np.linspace(0,1,nSamp)
     elif sampType =='GL':
        xi,wXI=pce.GaussLeg_ptswts(nSamp)  #on [-1,1]
+       xi_len=2.
+       xi0=-1.
     elif sampType=='Clenshaw':
        xi=nodes.Clenshaw_pts(nSamp)
+       xi_len=2.
+       xi0=-1.
+    elif sampType=='Clenshaw-Curtis':
+       l_=1+int(mt.log(nSamp-1)/mt.log(2))
+       xi=nodes.ClenshawCurtis_pts(l_)
     #map from reference range to actual range
-    qNodes=(range_[1]-range_[0])*xi+range_[0]
+    qNodes=(range_[1]-range_[0])*(xi-xi0)/xi_len+range_[0]
     return qNodes
 
 
