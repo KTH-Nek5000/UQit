c86184628cb28506e9704d555e30650fa6a8cbcb
Saleh <salehr@kth.se>
2020-08-21 15:01:54 +0200

    small modifications

--- core/writeUQ.py
+++ core/writeUQ.py
@@ -3,10 +3,8 @@
 #####################################################
 # Saleh Rezaeiravesh,   salehr@kth.se
 #----------------------------------------------------
-
+#
 def printRepeated(string_to_expand, length):
     """ Repeat string_to_extend up to a certain length """
     return (string_to_expand * (int(length/len(string_to_expand))+1))[:length]
-
-
-
+#
