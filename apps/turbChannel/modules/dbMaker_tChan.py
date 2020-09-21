63a28862a04630cc40e56cb1beb55107ed4a555d
Saleh <salehr@kth.se>
2020-09-21 12:09:10 +0200

    docstring finalized

--- apps/turbChannel/modules/dbMaker_tChan.py
+++ apps/turbChannel/modules/dbMaker_tChan.py
@@ -1,39 +1,50 @@
-############################################################
-# Construct databases for the channel flow simulation data
-############################################################
+##########################################################################
+# Construct databases from the QoIs of turbulent channel flow simulations
+##########################################################################
 # Saleh Rezaeiravesh,  salehr@kth.se
-#-----------------------------------------------------------
+#-------------------------------------------------------------------------
+#
 import nekProfsReader_tChan
 #
 def dbMakerCase_multiPar(prePath,caseName,interpOpts):
     """ 
-    Make a sorted list of databases for a channel flow simulations case <caseName> which have 2 or more uncertain parameters and are simulated by either Nek5000 or OpenFOAM.   
-    NOTE: A case contains several channel flow simulation.
-    NOTE: for OpenFOAM, interpOpts={}
-    -The database of each simulation included in the case contains statsitics profiles of channel flow.   
-    -Each channel flow simulation corresponds a unique combination of uncertain parameters. We assume tensor-product was considered when designing the simulations case. 
-    -Sorting is required to make sure our convention for tensor-product parameter samples is in place. (convention: the latest parameter should be in the outermost loop)
-    -First the db of statsitics profiles of all simulations included in a case are read. A list of these db's is created. Then the information about the simulations included in the case (name and associated parameter samples) are read and sorted according to the convention. Finally, the list of db's is accordingly sorted and the entries of the information-db is merged with that. 
+    Make a sorted list of databases (dicts) for a set of channel flow simulations \
+            which are conducted in a computer experiment conducted for two or more uncertain parameters.
+       * CFD simulations can be done by either Nek5000 or OpenFOAM.   
+       * The database of each simulation is a dict containing profiles of channel flow QoIs which are averaged over time and space.   
+       * Each channel flow simulation corresponds a unique sample of uncertain parameters. 
+         We assume tensor-product over parameter space when designing the experiment.
+       * Sorting is required to make sure our convention for tensor-product parameter samples is in place. 
+         Convention: Higher the parameter index, slower the loop changes: Fortran-like loop order.
+
+    Args:
+       `PrePath`: string
+          Path to the channel flow data
+       `caseName`: string
+          Name of the computer experiment (i.e. the set of channel flow simulations)
+       `interOpts`: dict
+          Options for interpolating the Nek5000 data from GLL points to arbitrary uniform mesh.
+          for OpenFoam, `interOpts`={}
+    
+    Returns:
+       `db`: List of dicts
+          Containing the QoIs of channel simulations in the computer experiment
     """
-    #(a) Read information about the case. This db is sorted according to our convention for tensor product (last parameter has the outermost loop)
     db_info=nekProfsReader_tChan.sortedCaseInfo(prePath,caseName)
-    nSamples=db_info['nSamples']   #list of number of samples 
+    nSamples=db_info['nSamples']   
     nSamplesProd=1
     for i in range(len(nSamples)):
         nSamplesProd*=nSamples[i]
-    #(b) Read in data in the order that oringinally ran (in Excel sheet)
-    db=[];  #list of databases of profiles
+    db=[]
     for i in range(nSamplesProd):
-        if len(interpOpts)==0:  #i.e. for OpenFOAM
+        if len(interpOpts)==0:
            nam=caseName+'_'+str(i+1)
            db1=ofProfsReader.dbCnstrctr_OF(prePath,nam)
-        else:    #i.e. Nek5000
+        else:    
            nam=caseName+'_'+str(i+1)
            db1=nekProfsReader_tChan.dbCnstrctr_NEK(prePath+nam,interpOpts)
         db1.update({'name':nam})        
         db.append(db1)
-    #(c) Sort list of profile databases in accordance with db_info + merge these two db's
-    print('... sorting the read-in databases according to our tensor-product convention.')
     db=nekProfsReader_tChan.sort_merge_dbs(db,db_info)
     return db,nSamples
 #
