c0d097976e9ade26884c078c27ee2fcef9df8b2d
Saleh <salehr@kth.se>
2020-10-22 11:28:10 +0200

    initial

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="UQit", # Replace with your own username
+    version="1.0.0",
+    author="Saleh Rezaeiravesh",
+    author_email="salehr@kth.se",
+    description="A Python Package for Uncertainty Quantification (UQ) in Computational Fluid Dynamics (CFD)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/KTH-Nek5000/UQit",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache License 2.0",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
